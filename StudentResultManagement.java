fc6e4488ac5b756aca93fd8deaf1c67969ab2483
theertha436 <theerthaesvar@gmail.com>
2026-04-13 16:17:43 +0530

    Add files via upload

--- StudentResultManagement.java
+++ StudentResultManagement.java
@@ -11,8 +11,8 @@ public class StudentResultManagement
 		System.out.println("Register Number: 23MIC0061");
 		System.out.println("Course Name: Computer Science and Engineering");
 		System.out.println("Subject Name: APPLICATION DEVELOPMENT AND DEPLOYMENT ARCHITECTURE");
-		System.out.println("Marks: 89");
-		System.out.println("Grade: B");
+		System.out.println("Marks: 99");
+		System.out.println("Grade: S");
 		System.out.println("Message: “Result Build Successful” ");
 	}
 }
