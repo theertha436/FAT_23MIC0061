837a024b5d8701349ec25d8dc36211c39d4be747
theertha436 <theerthaesvar@gmail.com>
2026-04-13 16:14:46 +0530

    Add files via upload

---
+++ StudentResultManagement.java
@@ -0,0 +1,18 @@
+package srma;
+
+import java.io.*;
+import java.util.*;
+
+public class StudentResultManagement
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Student Name: E. THEERTHA");
+		System.out.println("Register Number: 23MIC0061");
+		System.out.println("Course Name: Computer Science and Engineering");
+		System.out.println("Subject Name: APPLICATION DEVELOPMENT AND DEPLOYMENT ARCHITECTURE");
+		System.out.println("Marks: 89");
+		System.out.println("Grade: B");
+		System.out.println("Message: “Result Build Successful” ");
+	}
+}
